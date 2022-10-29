190c5282d9ed81167c9badfec81be4b0d898d42b
justa <justa@MacBook-Air-justyna.local>
2022-10-29 12:25:59 +0200

    intial files - compnents and routs

---
+++ src/app/recipes/recipe-details/recipe-details.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { Ingredient } from 'src/app/shared/ingredients.model';
+import { Recipe } from '../recipes.model';
+import { RecipesService } from '../recipes.service';
+
+@Component({
+  selector: 'app-recipe-details',
+  templateUrl: './recipe-details.component.html',
+  styleUrls: ['./recipe-details.component.css']
+})
+export class RecipeDetailsComponent implements OnInit {
+
+  recipe: Recipe;
+  id: number;
+  constructor(private recipeService: RecipesService, private router: Router, private route: ActivatedRoute) { }
+
+  ngOnInit() {
+    this.route.params.subscribe(
+      (params: Params) => {
+        this.id = +params['id'];
+        this.recipe = this.recipeService.getRecipe(this.id)
+      }
+    )
+  }
+
+  onAddToShoppingList() {
+    this.recipeService.addIngredientsToShoppingList(this.recipe.ingredients)
+  }
+
+  onEditRecipe() {
+    this.router.navigate(['edit'], { relativeTo: this.route })
+  }
+}
---
+++ src/app/recipes/recipes.service.ts
@@ -0,0 +1,27 @@
+import { EventEmitter, Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredients.model";
+import { ShoppingListService } from "../shopping-list/shoppingList.service";
+import { Recipe } from "./recipes.model";
+
+@Injectable()
+export class RecipesService {
+  recipeSelected = new EventEmitter<Recipe>()
+
+  private recipes: Recipe[] = [new Recipe('A test Recipe', 'This is simply a test for recipe', 'https://www.tasteaway.pl/wp-content/uploads/2020/03/kura.jpg', [new Ingredient('tomato', 2), new Ingredient('orange', 3)]), new Recipe('Another test Recipe', 'This is simply a test for another recipe', 'https://www.tasteaway.pl/wp-content/uploads/2020/03/kura.jpg', [new Ingredient('bread', 3), new Ingredient('pasta', 2)])];
+
+  constructor(private shoppingListService: ShoppingListService) {
+
+  }
+
+  getRecipes() {
+    return this.recipes.slice()
+  }
+
+  getRecipe(index: number) {
+    return this.recipes[index]
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients)
+  }
+}
