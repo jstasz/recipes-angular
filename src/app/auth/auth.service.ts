8650af2678b6b0e34506ad5147c2d00d612239d8
jstasz <jszajuk@gmail.com>
2022-11-15 11:20:15 +0100

    auto logout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -21,6 +21,7 @@ export interface AuthResponseData {
 export class AuthService {
 
   user = new BehaviorSubject<User>(null)
+  private tokenExpirationTimer: any;
 
   constructor(private http: HttpClient, private router: Router) { }
 
@@ -54,18 +55,36 @@ export class AuthService {
     }
 
     const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate))
-    this.user.next(loadedUser)
+
+    if (loadedUser.token) {
+      this.user.next(loadedUser)
+      const expirationDuration = new Date(userData._tokenExpirationDate).getTime() - new Date().getTime()
+      this.autoLogOut(expirationDuration)
+    }
   }
 
   logOut() {
     this.user.next(null)
     this.router.navigate(['/auth'])
+    localStorage.removeItem('userData')
+
+    if (this.tokenExpirationTimer) {
+      clearTimeout(this.tokenExpirationTimer)
+    }
+    this.tokenExpirationTimer = null
+  }
+
+  autoLogOut(expirationDuration: number) {
+    this.tokenExpirationTimer = setTimeout(() => {
+      this.logOut()
+    }, expirationDuration)
   }
 
   private handlingAuthentication(email: string, userId: string, token: string, expiresIn: number) {
     const expirationDate = new Date(new Date().getTime() + expiresIn * 1000)
     const user = new User(email, userId, token, expirationDate)
     this.user.next(user)
+    this.autoLogOut(expiresIn * 1000)
     localStorage.setItem('userData', JSON.stringify(user))
   }
 
